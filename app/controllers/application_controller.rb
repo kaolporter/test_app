d43e30ea0cf0a5b9484bcef309561ee4ae7e7655
kaolporter <124106905+kaolporter@users.noreply.github.com>
2024-08-20 11:18:48 -0600

    Initial commit and add root route

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,8 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  # Need to render a template from controller methods (actions)
+  allow_browser versions: :modern
+  def hello
+    render html: "Hello World!"
+  end
+end
---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,5 @@
+class PagesController < ApplicationController
+  # By convention this expects home.html.erb in the pages folder in views
+  def home
+  end
+end
